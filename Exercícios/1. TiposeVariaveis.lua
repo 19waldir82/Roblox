fae8e3165d2b92402f4ddd9fb1895caccfb0c48f
Waldir Aparecido da Silva <19waldir82@gmail.com>
2022-08-07 12:28:34 -0300

    Update 1. TiposeVariaveis.lua

--- Exercícios/1. TiposeVariaveis.lua
+++ Exercícios/1. TiposeVariaveis.lua
@@ -1,8 +1,39 @@
 --Declarando algumas variáveis
 local energia = 3
-local nome = "Arthur"
+local nome = "arthurlor13"
 local passouFase = false
 
 --Somando valores
 local maisEnergia = 4
 print(energia + maisEnergia)
+
+--Concatenando texto
+print("Nome do jogador: " .. nome)
+
+--Verificar tipos
+print(type(energia))
+print(type(nome))
+print(type(passouFase))
+
+--Operadores Relacionais
+print(energia == maisEnergia)
+print(energia ~= maisEnergia)
+print(energia > maisEnergia)
+
+--Nil
+nome = ""
+print(nome)
+nome = nil
+print(nome)
+
+
+
+
+
+
+
+
+
+
+
+
